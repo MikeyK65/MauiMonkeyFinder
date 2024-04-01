be7b120491f471776c39a35c7770c9b9fb65a08e
= <mikeyk65@hotmail.com>
2024-04-01 16:54:27 +0100

    first commit

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MonkeyFinder
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MonkeyFinder.ViewModel
+{
+    [INotifyPropertyChanged]
+    public partial class BaseViewModel
+    {
+        bool isBusy;
+        string title;
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (isBusy == value) return;
+                isBusy = value;
+                OnPropertyChanged();
+                //OnPropertyChanged(nameof(IsBusy));
+            }
+        }
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value) return;
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    }
+}
